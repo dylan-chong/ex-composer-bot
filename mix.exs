63d4b3b099c38b8838b94dbbedb11c286d0d2e30
Dylan Chong <dylan.chong@icloud.com>
2017-12-25 00:12:39 +1300

    Add coveralls

--- mix.exs
+++ mix.exs
@@ -8,7 +8,14 @@ defmodule ComposerBot.Mixfile do
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        "coveralls": :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+      ],
     ]
   end
 
@@ -36,6 +43,7 @@ defmodule ComposerBot.Mixfile do
     [
       {:credo, "~> 0.8.8", only: [:dev, :test]},
       {:mix_test_watch, "~> 0.5", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.7", only: :test},
     ]
   end
 
