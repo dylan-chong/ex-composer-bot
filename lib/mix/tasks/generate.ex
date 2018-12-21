77e8e10fce0506ef28e15381c3b18cbcc944a9ff
Dylan Chong <dylan.chong@icloud.com>
2018-12-21 13:40:53 +1300

    Minor refactor in the generate file

--- lib/mix/tasks/generate.ex
+++ lib/mix/tasks/generate.ex
@@ -6,17 +6,16 @@ defmodule Mix.Tasks.Generate do
   alias ExComposerBot.{PartimentoChordsGenerator, FileExport}
 
   @shortdoc "Generate some music"
-  def run(_) do
-    generate_something()
-  end
+  def run([output_path]), do: generate_something(output_path)
+  def run(_), do: generate_something()
 
   defp generate_something(path \\ default_file_path()) do
     bass = PartimentoChordsGenerator.generate_bass()
-    FileExport.to_lily_string([bass, bass])
-    FileExport.write_to(lily_string, path)
 
+    lily_string = FileExport.to_lily_string([bass, bass])
     IO.puts(lily_string)
 
+    FileExport.write_to(lily_string, path)
     IO.puts("\n")
     IO.puts("Outputted to file: #{path}")
   end
--- test/mix/tasks/generate_test.exs
+++ test/mix/tasks/generate_test.exs
@@ -3,10 +3,21 @@ defmodule MixTest.Tasks.Generate do
   alias Mix.Tasks.Generate
   import ExUnit.CaptureIO
 
-  test "does not crash" do
-    capture_io fn ->
+  test "does not crash when not given arguments" do
+    capture_io(fn ->
       Generate.run([])
-    end
+    end)
+  end
+
+  test "creates the given file" do
+    file_path = "tmp/test-file"
+    File.rm(file_path)
+
+    capture_io(fn ->
+      Generate.run([file_path])
+    end)
+
+    assert File.exists?(file_path)
+    assert String.length(File.read!(file_path)) > 0
   end
 end
-
