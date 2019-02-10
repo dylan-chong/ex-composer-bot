2701e2881e9979a105d4732872d90dc832e6563b
Dylan Chong <dylan.chong@icloud.com>
2019-02-10 21:21:06 +1300

    Add length test for generate_bass

--- test/ex_composer_bot/partimento_chords_generator_test.exs
+++ test/ex_composer_bot/partimento_chords_generator_test.exs
@@ -3,8 +3,16 @@ defmodule ExComposerBotTest.PartimentoChordsGenerator do
   use ExUnit.Case, async: true
   alias ExComposerBot.{PartimentoChordsGenerator, Voice, Note, Scale, Chord.RomanChord}
 
-  test "generate_bass/1 creates a voice containing multiple notes" do
-    %Voice{notes: [%Note{} | _]} = PartimentoChordsGenerator.generate_bass(number_of_notes: 3)
+  describe "generate_bass/1 creates" do
+    test "a voice containing multiple notes" do
+      %Voice{notes: [%Note{} | _]} = PartimentoChordsGenerator.generate_bass(number_of_notes: 3)
+    end
+
+    test "a list of length equal to the bass length" do
+      %Voice{notes: notes} = PartimentoChordsGenerator.generate_bass(number_of_notes: 3)
+
+      assert length(notes) == 3
+    end
   end
 
   describe "generate_chords/1 creates" do
